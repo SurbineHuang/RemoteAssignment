c85f177fe0530139b55dea691c870dc6540d6f94
SurbineHuang <SurbineHuang@SurbineHuangdeMacBook-Air-2.local>
2021-03-02 02:56:41 +0800

    update the file

--- week1/week1.playground/Pages/Control Flow.xcplaygroundpage/Contents.swift
+++ week1/week1.playground/Pages/Control Flow.xcplaygroundpage/Contents.swift
@@ -1,7 +1,31 @@
-//: [Previous](@previous)
 
-import Foundation
 
-var str = "Hello, playground"
+ //==== 1 ====
 
-//: [Next](@next)
+let lottoNumbers = [10, 9, 8, 7, 6, 5]
+
+print(lottoNumbers[0])
+
+for i in lottoNumbers {
+    print(i)
+}
+print("-")
+    
+for i in 3..<6 {
+    print(lottoNumbers[3])
+}
+
+////for _ in lottoNumbers  {
+//    print()
+//}
+    
+ //==== 2 ====
+
+//for i in 3..<6 {
+//    print(i*2)
+//}
+
+
+ //==== 3 ====
+
+
