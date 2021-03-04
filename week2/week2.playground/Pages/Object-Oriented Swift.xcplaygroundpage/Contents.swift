2de73cfcdcbc54fdd5f2678a1da7243f0f7f7feb
SurbineHuang <SurbineHuang@SurbineHuangdeMacBook-Air-2.local>
2021-03-05 00:04:25 +0800

    Update the file

--- week2/week2.playground/Pages/Object-Oriented Swift.xcplaygroundpage/Contents.swift
+++ week2/week2.playground/Pages/Object-Oriented Swift.xcplaygroundpage/Contents.swift
@@ -2,6 +2,101 @@
 
 import Foundation
 
-var str = "Hello, playground"
+//=== 1 ===
 
-//: [Next](@next)
+enum Gender {
+    case male
+    case female
+    case undefined
+}
+
+class Animal {
+    let gender: Gender
+    
+    init(gender: Gender) {
+        self.gender = gender}
+    
+    func eat() {
+        print("I eat everything!")
+    }
+}
+
+let animal = Animal(gender: .male)
+animal.eat()
+
+
+print("----------------")
+
+//=== 2 ===
+
+class Elephant: Animal {
+    override func eat() {
+        print("Eat grass")
+    }
+}
+let elephant = Elephant(gender: .male)
+elephant.eat()
+
+class Tiger: Animal {
+    override func eat() {
+        print("Eat meat")
+    }
+}
+let tiger = Tiger(gender: .male)
+tiger.eat()
+
+class Horse: Animal {
+    override func eat() {
+        print("Eat grass")
+    }
+}
+let horse = Horse(gender: .male)
+horse.eat()
+
+print("----------------")
+
+//=== 3 ===
+
+class Zoo {
+    
+    var weeklyHot: Animal
+    
+    init(weeklyHot: Animal) {
+        self.weeklyHot = weeklyHot
+    }
+}
+
+let zoo = Zoo(weeklyHot: Tiger(gender: .male))
+
+zoo.weeklyHot = tiger
+zoo.weeklyHot = elephant
+zoo.weeklyHot = horse
+
+
+//=== 4 ===
+
+//Struct 是 value type，結構、統計值。Ｃlass 是 reference type，物件導向
+//Struct不可以被繼承，Class可以被繼承
+//Struct有memeberwise initializer
+
+//=== 5 ===
+
+
+
+//=== 6 ===
+
+//Initializer 是準備要使用 class 或 struct 的實例過程，為實例裡的屬性設立一個初始值
+
+//=== 7 ===
+
+
+
+
+//=== 8 ===
+
+//reference type 是保存變數位置
+//value type 是保存變數的值
+
+
+
+
